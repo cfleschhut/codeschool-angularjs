44bcc8e697e5c06042cf552df7a182400a6a07e5
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-07-05 20:28:26 +0200

    initial generator-angular yo app

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularApp
+ * @description
+ * # angularApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
