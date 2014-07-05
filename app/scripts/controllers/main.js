e9e08375d0283470e0988d65c3641b9c8e56279b
Christian Fleschhut <weltrekordhalter@gmail.com>
2014-07-05 21:35:43 +0200

    complete codeschool angular level 1

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,34 @@
  */
 angular.module('angularApp')
   .controller('MainCtrl', function ($scope) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
   });
+
+/* StoreController */
+angular.module('angularApp')
+  .controller('StoreController', function() {
+
+    this.products = [
+      {
+        name: 'Product 1',
+        description: 'lorem ipsum dolor',
+        price: '2.95',
+        canPurchase: true,
+        soldOut: false
+      },
+      {
+        name: 'Product 2',
+        description: 'lorem ipsum dolor',
+        price: '3.95',
+        canPurchase: true,
+        soldOut: false
+      }
+    ];
+
+});
